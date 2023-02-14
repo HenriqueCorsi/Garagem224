74330a378743008e5b89a5f548fecd6675d57c56
Paulo Henrique <ph55@hotmail.com>
2023-02-14 20:34:31 -0300

    Adicionado Botão de Scroll

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+//Menu Header
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
+
+
+function ocultarBtnTop(){
+    const btn = document.querySelector('.btn-top');
+
+    if(window.scrollY > 10){
+        btn.style.display = 'flex';
+    }else{
+        btn.style.display = 'none';
+    }
+}
+
+// Botão de Scroll
+function btnTop(){
+    const btn = document.querySelector('.btn-top');
+
+    btn.addEventListener('click', function(){
+        window.scrollTo(0, 0);
+    })
+
+    document.addEventListener('scroll', ocultarBtnTop);
+}
+
+
