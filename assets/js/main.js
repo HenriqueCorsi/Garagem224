aa25ddf6be3725f7e19c69fabb7410c88dcddaaf
Paulo Henrique <ph55@hotmail.com>
2023-02-17 14:43:40 -0300

    Ajustes na responsividade na página contato

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ function menuShowSecond() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "../img/close_white_36dp.svg";
+        document.querySelector('.icon').src = "../img/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "../img/close_white_36dp.svg";
